234344bd91bb4a3690b0bbb26b7f6aa33e5721ca
iTormentor <toranders98@gmail.com>
2021-04-16 16:29:52 +0200

    Finished the classes ToDoMain and Project

--- src/main/java/no/ntnu/idata1002/Project.java
+++ src/main/java/no/ntnu/idata1002/Project.java
@@ -1,4 +1,98 @@
 package no.ntnu.idata1002;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * All existing projects should exist in the ToDoMain List over existing projects
+ */
 public class Project {
+
+    private String projectName;
+    private List<Task> taskList;
+    private boolean started = false;
+    private boolean finished = false;
+
+
+    /**
+     * Constructor for Project.
+     * Creates an empty list, that can be filled with Tasks
+     *
+     * @param name Name of the project as String
+     */
+    public Project(String name){
+        this.projectName = name;
+        taskList = new ArrayList<>();
+
+    }
+
+    /**
+     * Changes the status of the started variable
+     */
+    public void setFinished(boolean status){
+        this.finished = status;
+    }
+
+    /**
+     * Changes the status of the finished variable
+     */
+    public void setStarted(boolean status){
+        this.started = status;
+    }
+
+
+    /**
+     * Changes the name of the project
+     *
+     * @param projectName Name of the project as String
+     */
+    public void setProjectName(String projectName){
+        this.projectName = projectName;
+    }
+
+    /**
+     * Returns the name of the project
+     *
+     * @return Name of the project as String
+     */
+    public String getProjectName(){
+        return this.projectName;
+    }
+
+    /**
+     * Returns the value of the variable finished
+     *
+     * @return boolean value of 'finished'
+     */
+    public boolean getFinished(){
+        return this.finished;
+    }
+
+    /**
+     * Returns the value of the variable started
+     *
+     * @return boolean value of started
+     */
+    public boolean getStarted(){
+        return this.started;
+    }
+
+    /**
+     * Returns the taskList
+     *
+     * @return The list contained in the 'tasks' variable
+     */
+    public List<Task> getTaskListTask(){
+        return this.taskList;
+    }
+
+    /**
+     * Removes a task from the ArrayList taskList
+     *
+     * @param task The Task object to be removed
+     */
+    public void removeTask(Task task){
+        taskList.remove(task);
+    }
 }
---
+++ src/main/java/no/ntnu/idata1002/ToDoMain.java
@@ -0,0 +1,112 @@
+package no.ntnu.idata1002;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoMain {
+
+    private List<Project> projectList;
+
+    /**
+     * Creates a List that can be filled with projects
+     */
+    public ToDoMain(){
+        projectList = new ArrayList<>();
+    }
+
+
+    /**
+     * Fills up the application with various information.
+     * For testing purposes
+     */
+    //TODO Cant finish before the Task classes is finished
+    public void fillWithDummies(){
+        projectList.add(new Project("House-work"));
+        projectList.add(new Project("Garden-work"));
+    }
+
+
+    /**
+     * Returns all the existing projects in a list
+     *
+     * @return projectList as ArrayList
+     */
+    public List<Project> getAllProjects(){
+        return projectList;
+    }
+
+    /**
+     * Checks all existing projects for their 'started' and 'finished' variable.
+     * If started and not finished, they will be added to another list,
+     * which will then be returned
+     *
+     * @return All projects with 'started' as true and 'finished' as false, in an ArrayList
+     */
+    public List<Project> listOngoingProjects(){
+        List<Project> ongoingProjects = new ArrayList<>();
+        for (Project project : projectList) {
+            if(project.getStarted() && !project.getFinished()){
+                ongoingProjects.add(project);
+            }
+        }
+        return ongoingProjects;
+
+    }
+
+    /**
+     * Checks all existing projects for their 'finished' variable.
+     * If true, they will be added to another list, which will then
+     * be returned
+     *
+     * @return All projects marked as
+     */
+    public List<Project> listDoneProjects(){
+
+        List<Project> doneProjects = new ArrayList<>();
+        for (Project project : projectList) {
+            if(project.getFinished()){
+                doneProjects.add(project);
+            }
+        }
+
+        return doneProjects;
+    }
+
+
+    /**
+     * Checks all existing projects for their 'started' and 'finished' variable.
+     * If both is false, they will be added to another list,
+     * which will then be returned
+     *
+     * @return All projects with 'started' and 'finished' as false, in an ArrayList
+     */
+    public List<Project> listPlannedProjects(){
+        List<Project> plannedProjects = new ArrayList<>();
+
+        for (Project project : projectList) {
+            if (!project.getStarted() && !project.getFinished()){
+                plannedProjects.add(project);
+            }
+        }
+        return plannedProjects;
+    }
+
+    /**
+     * Adds an existing project to the list of all projects
+     *
+     * @param project The project to be added
+     */
+    public void addProject(Project project){
+
+        projectList.add(project);
+    }
+
+
+    /**
+     * Removes an existing project from the projectList
+     * @param project The project to be removed
+     */
+    public void removeProject(Project project){
+        projectList.remove(project);
+    }
+}
