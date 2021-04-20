484b58cb8759097d869b9979520b16f3bd95c0ee
Jonas Klinge Husøy <jonasklih@stud.ntnu.no>
2021-04-20 15:20:04 +0200

    Small changes

--- src/main/java/no/ntnu/idata1002/Main.java
+++ src/main/java/no/ntnu/idata1002/Main.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class App extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
